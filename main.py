54b372180e5ab1d3f3c13cdee91bb7f1137a4a95
Matthew Arofin <Matt.arofin@gmail.com>
2022-11-23 19:45:52 -0500

    Added working timer feature

--- main.py
+++ main.py
@@ -16,12 +16,18 @@ target_images = [[], [], []]
 targets = {1: [10, 5, 3],
            2: [12, 8, 5],
            3: [15, 12, 8, 3]}
-level = 3
+level = 1
 points = 0
 total_shots = 0
 mode = 0
 ammo = 0
+time_passed = 0
+time_remaining = 0
+counter = 1
 shot = False
+menu = True
+game_over = False
+pause = False
 
 for i in range(1, 4):
     bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
@@ -35,6 +41,21 @@ for i in range(1, 4):
         for j in range(1, 5):
             target_images[i - 1].append(pygame.transform.scale(
                 pygame.image.load(f'assets/targets/{i}/{j}.png'), (120 - (j * 18), 80 - (j * 12))))
+
+def draw_score():
+    points_text = font.render(f'Points: {points}', True, 'black')
+    screen.blit(points_text, (320, 660))
+    shots_text = font.render(f'Total Shots: {total_shots}', True, 'black')
+    screen.blit(shots_text, (320, 687))
+    time_text = font.render(f'Time Elapsed: {time_passed}', True, 'black')
+    screen.blit(time_text, (320, 714))
+    if mode == 0:
+        mode_text = font.render(f'Freeplay!', True, 'black')
+    if mode == 1:
+        mode_text = font.render(f'Ammo Remaining: {ammo}', True, 'black')
+    if mode == 2:
+        mode_text = font.render(f'Time Remaining: {time_remaining}', True, 'black')
+    screen.blit(mode_text, (320, 741))
 
 def draw_gun():
     mouse_pos = pygame.mouse.get_pos()
@@ -117,10 +138,28 @@ for i in range(4):
 run = True
 while run:
     timer.tick(fps)
+    if level != 0:
+        if counter < 60:
+            counter += 1
+        else:
+            counter = 1
+            time_passed += 1
+            if mode == 2:
+                time_remaining -= 1
 
     screen.fill('black')
     screen.blit(bgs[level -1], (0, 0))
     screen.blit(banners[level - 1], (0, HEIGHT - 200))
+    if menu:
+        level = 0
+        draw_menu()
+    if game_over:
+        level = 0
+        draw_game_over()
+    if pause:
+        level = 0
+        draw_pause()
+
     if level == 1:
         target_boxes = draw_level(one_coords)
         one_coords = move_level(one_coords)
@@ -141,6 +180,7 @@ while run:
             shot = False
     if level > 0:
         draw_gun()
+        draw_score()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -152,6 +192,10 @@ while run:
                 total_shots += 1
                 if mode == 1:
                     ammo -=1
+
+    if level > 0:
+        if target_boxes == [[], [], []] and level < 3:
+            level +=1
 
 
     pygame.display.flip()
